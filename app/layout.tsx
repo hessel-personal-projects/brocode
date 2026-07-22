a51ef33f014a966eca8f04c4f32703b864d85da9
Hessel Amann <hessel.amann@gmail.com>
2026-07-22 14:00:14 +0200

    feat(layout): add insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist_Mono } from "next/font/google";
 import { CRTOverlay } from "@/app/components/CRTOverlay";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
@@ -22,10 +24,15 @@ export default function RootLayout({
     <html lang="en" className={`${geistMono.variable} h-full antialiased`}>
       <body
         className="min-h-full flex flex-col"
-        style={{ background: "var(--color-bg)", color: "var(--color-phosphor)" }}
+        style={{
+          background: "var(--color-bg)",
+          color: "var(--color-phosphor)",
+        }}
       >
         <CRTOverlay />
         {children}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
